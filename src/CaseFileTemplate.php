9a9fabf7232f7b1164b99795756084e2e53ba371
janflora <j@nflora.dk>
2014-09-12 20:44:41 +0000

    Added advanced json decoding
    * Objects embedded in json responses can now be decoded
    * This allows for more complex responses and loading of complete object collections

--- src/CaseFileTemplate.php
+++ src/CaseFileTemplate.php
@@ -6,9 +6,15 @@ class CaseFileTemplate extends Entity
 	protected static $relativeUrl = 'casefiletype';
 
 	protected $name;
+	protected $documentTypes;
 
 	public function getName()
 	{
 		return $this->name;
+	}
+	
+	public function getDocumentTypes()
+	{
+		return $this->documentTypes;
 	}
 }
--- src/SignerType.php
+++ src/SignerType.php
@@ -5,9 +5,33 @@ class SignerType extends Entity
 {
 	protected static $relativeUrl = 'signertypes';
 	protected $role;
+	protected $upperLimit;
+	protected $lowerLimit;
+	protected $signOrder;
+	protected $conditions;
 
 	public function getName()
 	{
 		return $this->role;
+	}
+	
+	public function getLowerLimit()
+	{
+		return $this->lowerLimit;
+	}
+	
+	public function getUpperLimit()
+	{
+		return $this->upperLimit;
+	}
+
+	public function getSignOrder()
+	{
+		return $this->signOrder;
+	}
+	
+	public function getConditions()
+	{
+		return $this->conditions;
 	}
 }
