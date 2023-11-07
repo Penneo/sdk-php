fd2e1496154f78c10f833b1b2c02bae3c6825c0c
Ilmārs <132436248+ilmarspenneo@users.noreply.github.com>
2023-11-07 10:09:02 +0100

    documented use of url encoding method

--- src/OAuth/PKCE/PKCE.php
+++ src/OAuth/PKCE/PKCE.php
@@ -11,6 +11,12 @@ class PKCE
         return $this->base64UrlEncode(pack('H*', $random));
     }
 
+    /**
+     * We use an url-safe base64 encoding method so that the code challenge and its verifier can be passed safely as a
+     * GET parameter to web requests.
+     *
+     * @see https://datatracker.ietf.org/doc/html/rfc4648#section-5
+     */
     private function base64UrlEncode(string $str): string
     {
         return rtrim(strtr(base64_encode($str), '+/', '-_'), '=');
@@ -19,8 +25,7 @@ class PKCE
     public function getCodeChallenge(string $codeVerifier): CodeChallenge
     {
         return new CodeChallenge(
-            $this->base64UrlEncode(pack('H*', hash('sha256', $codeVerifier))),
-            CodeChallenge::S256
+            $this->base64UrlEncode(pack('H*', hash('sha256', $codeVerifier)))
         );
     }
 }
